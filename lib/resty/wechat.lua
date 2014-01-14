51a86e82e14e38556ca80a5d076f259b6034e44e
plc1989 <plc1989@gmail.com>
2014-01-14 20:40:33 +0800

    checkin the code parsing recieved xml body

--- lib/resty/wechat.lua
+++ lib/resty/wechat.lua
@@ -104,7 +104,7 @@ local rcvmsgfmt = {
 }
 
 local sndmsgfmt = {
-            --{ "nodename", "childnodetype", { ... }, s=[true or false] }
+           --{ "nodename", "childnodetype", { ... }, s=[true or false] }
     common = {
               {"tousername", "c"},
               {"fromusername", "c"},
@@ -162,6 +162,151 @@ local mt = { __index = _M }
 local lib = ffi.load("xml2")
 
 local str_type = ffi.typeof("uint8_t[?]")
+
+
+local function _parse_key(nodePtr, key, rcvmsg)
+    local node = nodePtr.node
+    local name = ffi_str(node[0].name)
+    local optional = (type(key) == "table") and true or false
+    local k = optional and key[1] or key
+
+    if lower(name) ~= k then -- case insensitive
+        if not optional then
+            return nil, "invalid node name"
+        else
+            return true
+        end
+    end
+
+    if node[0].type ~= lib.XML_ELEMENT_NODE then
+        return nil, "invalid node type"
+    end
+
+    node = node[0].children
+    if node == nil then
+        return nil, "invalid subnode"
+    end
+
+    if node[0].type ~= lib.XML_TEXT_NODE
+       and node[0].type ~= lib.XML_CDATA_SECTION_NODE then
+        return nil, "invalid subnode type"
+    end
+
+    rcvmsg[k] = ffi_str(node[0].content)
+
+    node = node[0].parent
+    if node[0].next ~= nil then
+        node = node[0].next
+    end
+    nodePtr.node = node
+
+    return rcvmsg[k]
+end
+
+
+local function _parse_keytable(nodePtr, keytable, rcvmsg)
+    for i = 1, #keytable do
+        local key = keytable[i]
+
+        local value, err = _parse_key(nodePtr, key, rcvmsg)
+        if err then
+            return nil, err
+        end
+    end
+
+    return true
+end
+
+
+local function _retrieve_keytable(nodePtr, key, rcvmsg)
+    local root = rcvmsgfmt.msgtype
+
+    while true do
+        if not root[key] then
+            return nil, "invalid key"
+        end
+
+        -- indicates that no subkeys present
+        if not rcvmsgfmt[key] then
+            break
+        end
+
+        local value, err = _parse_key(nodePtr, key, rcvmsg)
+        if err then
+            return nil, err
+        end
+
+        root = rcvmsgfmt[key]
+        key = value
+    end
+
+    return root[key]
+end
+
+
+local function _parse_xml(node, rcvmsg)
+    local keytable, ok, err
+
+    -- element node named xml is expected
+    if node[0].type ~= lib.XML_ELEMENT_NODE
+       or ffi_str(node[0].name) ~= "xml" then
+        return nil, "invalid xml title"
+    end
+
+    if node[0].children == nil then
+        return nil, "invalid xml content"
+    end
+
+    -- parse common components
+    local nodePtr = { node = node[0].children }
+    keytable = rcvmsgfmt.common
+
+    ok, err = _parse_keytable(nodePtr, keytable, rcvmsg)
+    if not ok then
+        return nil, "common parse failed :" .. err
+    end
+
+    -- retrieve msgtype-specific keytable
+    keytable, err = _retrieve_keytable(nodePtr, rcvmsg.msgtype, rcvmsg)
+    if err then
+        return nil, "retrieve keytable failed :" .. err
+    end
+
+    -- parse msgtype-specific components
+    ok, err = _parse_keytable(nodePtr, keytable, rcvmsg)
+    if not ok then
+        return nil, "msgtype-specific parse failed :" .. err
+    end
+
+    return true
+end
+
+
+function _M.parse(self)
+    local doc = ffi.new("struct _xmlDoc *")
+    local node = ffi.new("struct _xmlNode *")
+    local body = self.body
+    local rcvmsg = self.rcvmsg
+
+    doc = lib.xmlReadMemory(body, #body, nil, nil, 0)
+    if doc == nil then
+        return nil, "invalid xml data"
+    end
+
+    -- root node
+    node = doc[0].children
+    ok, err = _parse_xml(node, rcvmsg)
+
+    -- cleanup used memory anyway
+    lib.xmlFreeDoc(doc)
+    lib.xmlCleanupParser()
+
+    if not ok then
+        return nil, err
+    end
+
+    return true
+end
 
 
 local function _to_hex(s)
